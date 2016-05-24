2605c1f1632e98df7a2860439277a8aaf90cd941
Rodrigo Alves <rodrigo.f.a.alves@gmail.com>
2016-05-24 17:54:41 +0100

    heroes form

--- app/app.component.js
+++ app/app.component.js
@@ -9,13 +9,15 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var hero_form_component_1 = require('./hero-form.component');
 var AppComponent = (function () {
     function AppComponent() {
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular 2 App</h1>'
+            template: '<hero-form></hero-form>',
+            directives: [hero_form_component_1.HeroFormComponent]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
